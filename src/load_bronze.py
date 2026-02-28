f8e396c22b0e07831056b1dd0e13d4c0f0d5e69e
overseersnowfall <mps5501919@gmail.com>
2026-02-28 08:29:50 +0000

    Completed Bronze layer ingestion pipeline with dynamic CSV loading and logging

--- src/load_bronze.py
+++ src/load_bronze.py
@@ -4,8 +4,7 @@ import pandas as pd
 from datetime import datetime
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
-import os
-print("Current working directory:", os.getcwd())
+from pathlib import Path
 
 # ---------------------------------------------------
 # Load environment variables
@@ -36,19 +35,24 @@ DATABASE_URL = (
 
 engine = create_engine(DATABASE_URL)
 
+# Base directory (project root)
+BASE_DIR = Path(__file__).resolve().parent.parent # go two levels up to get to project root ecommerce-data-pipeline-project/
+RAW_PATH = BASE_DIR / "data" / "raw" # redirect to ecommerce-data-pipeline-project/data/raw/
+
 # ---------------------------------------------------
 # Test connection
 # ---------------------------------------------------
-def test_connection():
-    try:
-        with engine.connect() as connection:
-            result = connection.execute(text("SELECT 1"))
-            logging.info("Database connection successful.")
-    except Exception as e:
-        logging.error(f"Database connection failed: {e}")
+# def test_connection():
+#     try:
+#         with engine.connect() as connection:
+#             result = connection.execute(text("SELECT 1"))
+#             logging.info("Database connection successful.")
+#     except Exception as e:
+#         logging.error(f"Database connection failed: {e}")
 
 def load_single_csv(file_path, table_name):
     try:
+        starttime = datetime.now()
         logging.info(f"Loading file: {file_path}")
 
         df = pd.read_csv(file_path)
@@ -62,19 +66,31 @@ def load_single_csv(file_path, table_name):
             name=table_name,
             con=engine,
             schema="bronze_layer",
-            if_exists="append",
+            if_exists="replace",
             index=False
         )
 
+        endtime = datetime.now()
+        duration = (endtime - starttime).total_seconds()
         logging.info(f"Loaded {row_count} rows into bronze_layer.{table_name}")
+        logging.info(f"Loading time: {duration:.2f} seconds")
 
     except Exception as e:
         logging.error(f"Failed to load {file_path}: {e}")
 
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    #test_connection()
+
+    for file in RAW_PATH.glob("*.csv"):
+        # Extract clean table name
+        table_name = file.stem
+        table_name = table_name.replace("olist_", "")
+        table_name = table_name.replace("_dataset", "")
+
+        load_single_csv(file, table_name)
+
+
 if __name__ == "__main__":
-    test_connection()
-
-    file_path = "data/raw/olist_customers_dataset.csv"
-    table_name = "olist_customers_dataset"
-
-    load_single_csv(file_path, table_name)
+    main()
